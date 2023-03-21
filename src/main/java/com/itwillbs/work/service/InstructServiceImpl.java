de360795060c81a1cf69477012ed893bd71a898c
LEE <LEE@DESKTOP-P9IE25J>
2023-03-21 09:10:18 +0900

    작업지시

--- src/main/java/com/itwillbs/work/service/InstructServiceImpl.java
+++ src/main/java/com/itwillbs/work/service/InstructServiceImpl.java
@@ -1,8 +1,18 @@
 package com.itwillbs.work.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.itwillbs.work.domain.InstructDTO;
 
 @Service 
 public class InstructServiceImpl implements InstructService{
 
+	@Override
+	public List<InstructDTO> instrList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
